bfae87a7929aaf1279ced2ff37a7c065c7d9d7b7
Patrick Peixoto <patrickpeixoto.web@gmail.com>
2022-04-12 22:25:51 -0300

    Deletando as Postagens

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,19 +8,47 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    
-    public function index(){
+
+    public function index()
+    {
         $posts = Post::get();
-        return view('admin.posts.index',compact('posts'));
+        return view('admin.posts.index', compact('posts'));
     }
 
-    public function create(){
+    public function create()
+    {
         return view('admin.posts.create');
     }
 
-    public function store(StoreUpdatePost $request){
+    public function store(StoreUpdatePost $request)
+    {
         Post::create($request->all());
 
         return redirect()->route('posts.index');
+    }
+
+    public function show($id)
+    {
+        // Post::where('id', $id)->get();
+        //$post = Post::where('id', $id)->first();
+        $post = Post::find($id);
+
+        if (!$post) {
+            return redirect()->route('posts.index');
+        }
+
+        return view('admin.posts.show', compact('post'));
+    }
+
+    public function destroy($id){
+        if (!$post = Post::find($id)) {
+            return redirect()->route('posts.index');
+        }
+
+        $post->delete();
+
+        return redirect()
+                    ->route('posts.index')
+                    ->with('message','Post Deletado com Sucesso!');
     }
 }
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,7 +1,14 @@
+@if (session('message'))
+    <div>{{ session('message') }}</div>
+@endif
+
 <h1>Posts</h1>
 <a href="{{ route('posts.create') }}">Criar novo Post</a>
 <hr/>
 
 @foreach ($posts as $post)
-    <p>{{ $post->title }}</p>
+    <p>
+        {{ $post->title }} - 
+        [ <a href="{{ route('posts.show',['id' => $post->id]) }}">Ver</a> ]
+    </p>
 @endforeach
